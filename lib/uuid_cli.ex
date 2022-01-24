43753eb715553771a47b880f80eae4399bf32656
corka149 <corka149@mailbox.org>
2022-01-24 22:21:11 +0100

    Add cut

--- lib/uuid_cli.ex
+++ lib/uuid_cli.ex
@@ -19,13 +19,31 @@ defmodule UuidCli do
           help: "Activates verbose mode",
           multiple: false
         ]
+      ],
+      options: [
+        chars: [
+          value_name: "CHARS",
+          short: "-c",
+          long: "--chars",
+          help: "Amount of characters",
+          parser: fn n ->
+            case Integer.parse(n) do
+              :error -> {:error, "Must be integer"}
+              {chars, ""} -> {:ok, chars}
+            end
+          end,
+          default: -1
+        ]
       ]
     )
     |> Optimus.parse!(argv)
     |> debug()
     |> generate()
+    |> cut()
     |> IO.puts()
   end
+
+  # ===== ===== CMD ===== =====
 
   defp debug(
          %Optimus.ParseResult{
@@ -39,23 +57,42 @@ defmodule UuidCli do
     parse_result
   end
 
-  defp generate(%Optimus.ParseResult{
-         args: %{},
-         flags: %{random_case: true},
-         options: %{}
-       }) do
-    UUID.uuid4()
-    |> String.splitter("")
-    |> Enum.map(&random_case/1)
+  defp generate(
+         %Optimus.ParseResult{
+           flags: %{random_case: true}
+         } = parse_result
+       ) do
+    uuid =
+      UUID.uuid4()
+      |> String.splitter("")
+      |> Enum.map(&random_case/1)
+      |> Enum.join()
+
+    {uuid, parse_result}
   end
 
-  defp generate(%Optimus.ParseResult{
-         args: %{},
-         flags: %{random_case: false},
-         options: %{}
-       }) do
-    UUID.uuid4()
+  defp generate(
+         %Optimus.ParseResult{
+           flags: %{random_case: false}
+         } = parse_result
+       ) do
+    {UUID.uuid4(), parse_result}
   end
+
+  defp cut(
+         {uuid,
+          %Optimus.ParseResult{
+            options: %{chars: chars}
+          }}
+       ) do
+    if chars > 0 do
+      uuid |> String.splitter("") |> Enum.take(chars + 1)
+    else
+      uuid
+    end
+  end
+
+  # ===== ===== HELPER ===== =====
 
   defp random_case(char) when is_bitstring(char) do
     if :rand.uniform() < 0.5 do
